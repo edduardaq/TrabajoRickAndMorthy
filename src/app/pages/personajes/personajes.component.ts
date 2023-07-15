e8f265924dd3b3ade71ecc7fdeb6226af9a80365
edduardaq <Andresquiros.edgmail.com>
2023-07-15 09:41:45 -0600

    first commit

---
+++ src/app/pages/personajes/personajes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-personajes',
+  templateUrl: './personajes.component.html',
+  styleUrls: ['./personajes.component.scss']
+})
+export class PersonajesComponent {
+
+}
